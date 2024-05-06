e977ae3f2b54317e058dce21b9e9f39e33ea19dc
melli736 <118066463+melli736@users.noreply.github.com>
2024-05-06 15:44:32 +0200

    init

---
+++ src/main/java/mwallpach/springboot/DezsysGek863DomMongoDbWallpach4BhitApplication.java
@@ -0,0 +1,13 @@
+package mwallpach.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DezsysGek863DomMongoDbWallpach4BhitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DezsysGek863DomMongoDbWallpach4BhitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mwallpach/springboot/DezsysGek863DomMongoDbWallpach4BhitApplicationTests.java
@@ -0,0 +1,13 @@
+package mwallpach.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DezsysGek863DomMongoDbWallpach4BhitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
